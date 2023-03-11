0d73571f95df25ecfdd0f68dfeeb24a155c19fbd
YuYu3346 <75521348+YuYu3346@users.noreply.github.com>
2023-03-11 16:45:18 +0800

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+	int a;
+	double b;	
+	cin >> a;
+	b = a * 1.6;
+	cout << b << endl;
+	system("pause");
+	return 0;
+}
