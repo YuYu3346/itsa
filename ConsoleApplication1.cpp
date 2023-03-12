8bfcd27f9552fcaf60c5037f900ebedb70f9693c
YuYu3346 <75521348+YuYu3346@users.noreply.github.com>
2023-03-12 15:23:52 +0800

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+	int input = 0;
+
+	cin >> input;
+	switch (input / 3)
+	{
+	case 1:cout << "Spring" << endl; break;
+	case 2:cout << "Summer" << endl; break;
+	case 3:cout << "Autumn" << endl; break;
+	default:cout << "Winter" << endl;
+	}
+	system("pause");
+	return 0;
+}
