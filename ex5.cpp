96fe5be127acc6fb38e417e1732042a60f20b5e8
YuYu3346 <75521348+YuYu3346@users.noreply.github.com>
2023-03-12 15:44:03 +0800

    Add files via upload

---
+++ ex5.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cin >> num;
+
+    if (num < -128 || num > 127) {
+        cout << "error！" << endl;
+        return 0;
+    }
+
+    for (int i = 7; i >= 0; i--) {
+        cout << ((num >> i) & 1);
+    }
+    cout << endl;
+
+    return 0;
+}
