abb1cb68bd955b387745b698b6aa6305eee48d67
YuYu3346 <75521348+YuYu3346@users.noreply.github.com>
2023-03-12 16:12:32 +0800

    Add files via upload

---
+++ itsa/ex3/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,15 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+	int inputx = 0, inputy = 0;
+	while (cin >> inputx >> inputy)
+	{
+		if (inputx * inputx + inputy * inputy <= 10000) cout << "inside" << endl;
+
+		else cout << "outside" << endl;
+	}
+	system("pause");
+	return 0;
+}
