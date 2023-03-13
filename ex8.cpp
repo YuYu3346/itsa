34d8e7ebd494a0631cf3528e7f8fc926ede1fe2a
YuYu3346 <75521348+YuYu3346@users.noreply.github.com>
2023-03-14 02:34:42 +0800

    Add files via upload

---
+++ ex8.cpp
@@ -0,0 +1,35 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    int i;
+    int counter;
+
+    while (cin >> n)
+    {
+        counter = 0;
+
+        i = 1;
+        while (i <= n)
+        {
+            if (n % i == 0)
+            {
+                counter = counter + 1;
+            }
+            i = i + 1;
+        }
+
+        if (counter == 2)
+        {
+            cout << "yes" << endl;
+        }
+        else
+        {
+            cout << "no" << endl;
+        }
+    }
+
+    return 0;
+}
